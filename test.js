e3140ab526ae2d44ee38d698fea839a058979f93
Rafael Villegas <rafael.villegas@POLYGROUPMX.com>
2022-02-21 10:10:04 -0700

    Mi primer commit

---
+++ test.js
@@ -0,0 +1,12 @@
+/*
+var http = require('http');
+http.createServer(function(req,res){
+    res.write('Hello World');
+    res.end();
+}).listen(8080);   */
+const names = 'Hello'
+
+console.log("Node is working");
+console.log('Node is %s','working');
+
+console.log('node %s', names);
